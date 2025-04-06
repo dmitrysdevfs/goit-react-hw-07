2bbbcae433f119f018fca6af5efd94123b8da2cc
Dmytro <d.salyakin@gmail.com>
2025-04-06 14:04:57 +0300

    Remove Redux Persist and related code

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { persistor, store } from './redux/store';
+import { store } from './redux/store';
 import 'modern-normalize';
 import App from './components/App/App';
 import './index.css';
@@ -10,9 +9,7 @@ import './index.css';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
+      <App />
     </Provider>
   </StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,38 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 import filtersReducer from './filtersSlice';
 import contactsReducer from './contactsSlice';
-
-const persistedContactsReducer = persistReducer(
-  {
-    key: 'visible_contacts',
-    storage,
-    whitelist: ['items'],
-  },
-  contactsReducer
-);
 
 export const store = configureStore({
   reducer: {
     filters: filtersReducer,
-    contacts: persistedContactsReducer,
+    contacts: contactsReducer,
   },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
 });
-
-export const persistor = persistStore(store);
